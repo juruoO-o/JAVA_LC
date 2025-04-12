509565785820064a2fd6fc874e1536c74aac3413
juruoO-o <513530877@qq.com>
2025-04-12 22:47:42 +0800

    2025.4.12

--- completed/LC295.java
+++ completed/LC295.java
@@ -1,91 +1,77 @@
+import javax.management.relation.InvalidRelationTypeException;
+import javax.print.attribute.standard.PrinterURI;
+import javax.swing.*;
+import java.awt.image.AreaAveragingScaleFilter;
+import java.lang.reflect.Parameter;
 import java.util.*;
 
-//TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
-public class Main {
-    public static void main(String[] args) {
-        int[] nums1 = {1, 3, -1, -3, 5, 3, 6, 7};
-        int[] nums2 = {1, 2, 3, 4};
-        String s = "-2+ 1";
-        int[][] interval = {
-                {1, 3}, {8, 10}, {15, 18}, {2, 6}
+public class LC {
+    public static void main(String[] args) throws InterruptedException {
+        int[][] board = {{-1, -1, -1, -1, -1, -1}, {-1, -1, -1, -1, -1, -1}, {-1, -1, -1, -1, -1, -1}, {-1, 35, -1, -1, 13, -1}, {-1, -1, -1, -1, -1, -1}, {-1, 15, -1, -1, -1, -1}};
+        int[] nums = {8, 8};
+        int[][] obs = new int[][]{
+                {2, 2}
         };
-        TreeNode root = new TreeNode(1000000000,
-                new TreeNode(1000000000,
-                        new TreeNode(294967296,
-                                new TreeNode(1000000000,
-                                        new TreeNode(1000000000,
-                                                new TreeNode(1000000000), null), null), null), null), null);
-        char[][] board = {
-                {'A', 'B', 'C', 'E'},
-                {'S', 'F', 'C', 'S'},
-                {'A', 'D', 'E', 'E'}
-        };
-        int[][] arr = {
-                {1, 3, 5, 7},
-                {10, 11, 16, 20},
-                {23, 30, 34, 60}
-        };
-        int[][] arr1 = {{1, 3}};
-        int[] height = {4,1,-1,2,-1,2,3};
-        MedianFinder medianFinder = new MedianFinder();
-        medianFinder.addNum(1);
-        medianFinder.addNum(2);
-        medianFinder.findMedian();
-        medianFinder.addNum(3);
-        medianFinder.findMedian();
+        new Solution().robot("URR", obs, 3, 2);
 
     }
 }
 
-class MedianFinder {
-    PriorityQueue<Integer> pqbig = new PriorityQueue<>((a,b)->a-b);
-    PriorityQueue<Integer> pqsmall = new PriorityQueue<>((a,b)->b-a);
-    int cnt=0;
-    public MedianFinder() {
+class Pair<F, S> {
+    F first;
+    S second;
 
+    Pair(F f, S s) {
+        first = f;
+        second = s;
+    }
+}
+
+class Tri<F, S, T> {
+    F first;
+    S second;
+    T third;
+
+    Tri(F f, S s, T t) {
+        first = f;
+        second = s;
+        third = t;
+    }
+}
+
+class MedianFinder {
+    int cnt=0;
+    PriorityQueue<Integer> less,more;
+
+    public MedianFinder() {
+        less = new PriorityQueue<>((a,b)->b-a);
+        more = new PriorityQueue<>();
     }
 
     public void addNum(int num) {
         cnt++;
-        if (pqbig.size()==0){
-            pqbig.add(num);
-        }else if (pqsmall.size()==0){
-            int left = pqbig.peek();
-            if (left<num){
-                pqsmall.add(num);
+        if ((cnt&1)==1){
+            if (more.isEmpty() ||num<more.peek()){
+                less.add(num);
             }else{
-                pqbig.poll();
-                pqbig.add(num);
-                pqsmall.add(left);
+                less.add(more.poll());
+                more.add(num);
             }
         }else{
-            int left = pqbig.peek();
-            int right = pqsmall.peek();
-            if (cnt%2==0){
-                if (num>left){
-                    pqsmall.add(num);
-                }else{
-                    pqbig.poll();
-                    pqbig.add(num);
-                    pqsmall.add(left);
-                }
+            if (less.peek()>num){
+                more.add(less.poll());
+                less.add(num);
             }else{
-                if (num<right){
-                    pqbig.add(num);
-                }else{
-                    pqsmall.poll();
-                    pqsmall.add(num);
-                    pqbig.add(right);
-                }
+                more.add(num);
             }
         }
     }
 
     public double findMedian() {
-        if (cnt%2==1){
-            return pqbig.peek();
+        if ((cnt&1)==1){
+            return (double) less.peek();
         }else{
-            return (pqbig.peek()+pqsmall.peek())/2.0;
+            return (less.peek()+ more.peek())/2.0;
         }
     }
 }
