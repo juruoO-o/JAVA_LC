db787a477c0fc2143814c681d756cf72f402a7f7
juruo <513503877@qq.com>
2025-07-10 21:09:11 +0800

    2025.7.10继续-hot100写到42接雨水

--- src/LC.java
+++ src/LC.java
@@ -6,24 +6,35 @@ import java.util.stream.IntStream;
 
 public class LC {
     public static void main(String[] args) {
-        new Solution();
+        new Solution().trap(new int[]{0,1,0,2,1,0,1,3,2,1,2,1});
     }
 }
 
 
 class Solution {
-    public int lengthOfLIS(int[] nums) {
-        int ans = -1;
-        int len = nums.length;
-        int[] dp = new int[len];
-        for (int i = 0; i < dp.length; i++) {
-            dp[i] = 1;
-            for (int j = i - 1; j >= 0; j--) {
-                if (nums[i]>nums[j]){
-                    dp[i] = Math.max(dp[i],dp[j]+1);
-                }
+    public int trap(int[] height) {
+        int len = height.length;
+        int[] prefix = new int[len], postfix = new int[len];
+        for (int i = 0; i < height.length; i++) {
+            if (i - 1 >= 0) {
+                prefix[i] = Math.max(height[i], prefix[i - 1]);
+            } else {
+                prefix[i] = height[i];
             }
-            ans = Math.max(ans,dp[i]);
+        }
+        for (int i = len - 1; i >= 0; --i) {
+            if (i + 1 < len) {
+                postfix[i] = Math.max(postfix[i + 1], height[i]);
+            } else {
+                postfix[i] = height[i];
+            }
+        }
+        int ans = 0;
+        for (int i = 1; i < len - 1; i++) {
+            int high = Math.min(prefix[i - 1], postfix[i + 1]);
+            if (high>height[i]){
+                ans+=high-height[i];
+            }
         }
         return ans;
     }
