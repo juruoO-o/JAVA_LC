d7b6366ed92f79054f548157ae240471f62f95dc
juruo <513503877@qq.com>
2025-07-12 23:06:25 +0800

    2025.7.11太忙，7.12继续，只写了一个lc3，丢人

--- src/LC.java
+++ src/LC.java
@@ -6,35 +6,33 @@ import java.util.stream.IntStream;
 
 public class LC {
     public static void main(String[] args) {
-        new Solution().trap(new int[]{0,1,0,2,1,0,1,3,2,1,2,1});
+        new Solution().lengthOfLongestSubstring("abcabcbb");
     }
 }
 
 
 class Solution {
-    public int trap(int[] height) {
-        int len = height.length;
-        int[] prefix = new int[len], postfix = new int[len];
-        for (int i = 0; i < height.length; i++) {
-            if (i - 1 >= 0) {
-                prefix[i] = Math.max(height[i], prefix[i - 1]);
-            } else {
-                prefix[i] = height[i];
-            }
-        }
-        for (int i = len - 1; i >= 0; --i) {
-            if (i + 1 < len) {
-                postfix[i] = Math.max(postfix[i + 1], height[i]);
-            } else {
-                postfix[i] = height[i];
-            }
-        }
+    public int lengthOfLongestSubstring(String s) {
+        int left = 0, right = 0;
+        int[] cnt = new int[128];
+        char[] arr = s.toCharArray();
+        int len = arr.length;
         int ans = 0;
-        for (int i = 1; i < len - 1; i++) {
-            int high = Math.min(prefix[i - 1], postfix[i + 1]);
-            if (high>height[i]){
-                ans+=high-height[i];
+        while (right < len) {
+            while (right < len && (cnt[arr[right]] ==0 || left==right)) {
+                cnt[arr[right]]++;
+                right++;
             }
+            ans = Math.max(ans, right - left);
+            if (right == len) {
+                break;
+            }
+            cnt[arr[right]]++;
+            while (cnt[arr[right]]==2){
+                cnt[arr[left]]--;
+                left++;
+            }
+            right++;
         }
         return ans;
     }
