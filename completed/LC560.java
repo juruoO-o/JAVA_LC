f6f832d65994044efc07b1e5148579a39ddf2b9f
juruo <513503877@qq.com>
2025-07-28 22:10:53 +0800

    2025.7.28

--- completed/LC560.java
+++ completed/LC560.java
@@ -1,38 +1,24 @@
-import java.awt.image.AreaAveragingScaleFilter;
-import java.net.Inet4Address;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
-//TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
-public class Main {
+public class LC {
     public static void main(String[] args) {
-        int[] nums1 = {1, 3, 2, 5, 25, 24, 5};
-        int[] nums2 = {1, 1, 1};
-        String s = "-2+ 1";
-        TreeNode root = new TreeNode(1, new TreeNode(2), new TreeNode(3, new TreeNode(4), new TreeNode(5)));
-        new Solution().subarraySum(nums2, 2);
-        String string = new Codec().serialize(root);
-        TreeNode newRoot = new Codec().deserialize(string);
+        new Solution().subarraySum(new int[]{1,1,1},2);
     }
 }
+
 class Solution {
     public int subarraySum(int[] nums, int k) {
+        int prefix=0;
         Map<Integer,Integer> map = new HashMap<>();
-        int cnt=0,sum=0;
-        int ans=0;
         map.put(0,1);
-        for (int i = 0; i < nums.length; i++) {
-            sum+=nums[i];
-            Integer i1 = map.get(sum - k);
-            if(i1!=null){
-                ans+=i1;
-            }
-            Integer i2 = map.get(sum);
-            if (i2==null){
-                map.put(sum,1);
-            }else{
-                map.put(sum,i2+1);
-            }
+        int ans=0;
+        for (int num : nums) {
+            prefix+=num;
+            ans+=map.getOrDefault(prefix-k,0);
+            map.put(prefix,map.getOrDefault(prefix,0)+1);
         }
+
         return ans;
     }
 }
--- completed/LC76.java
+++ completed/LC76.java
@@ -1,73 +1,45 @@
-import com.sun.security.auth.UnixNumericGroupPrincipal;
-
-import java.awt.image.AreaAveragingScaleFilter;
-import java.nio.charset.StandardCharsets;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
-//TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
-public class Main {
+public class LC {
     public static void main(String[] args) {
-        int[] nums1 = {1, 3, -1, -3, 5, 3, 6, 7};
-        int[] nums2 = {1, 2, 3, 4};
-        String s = "-2+ 1";
-        int[][] interval = {
-                {1, 3}, {8, 10}, {15, 18}, {2, 6}
-        };
-        char[][] board = {
-                {'A', 'B', 'C', 'E'},
-                {'S', 'F', 'C', 'S'},
-                {'A', 'D', 'E', 'E'}
-        };
-        int[][] arr = {
-                {1, 2, 3},
-                {4, 5, 6},
-                {7, 8, 9}
-        };
-        int[][] arr1 = {{1, 3}};
-        int[] height = {1, 2, 87, 87, 87, 2, 1};
-        String[] strs = {"word", "good", "best", "word"};
-        new Solution().findSubstring("wordgoodgoodgoodbestword", strs);
+        new Solution().minWindow("a", "b");
     }
 }
 
+
 class Solution {
     public String minWindow(String s, String t) {
+        if (s.length() < t.length()) {
+            return "";
+        }
         int[] cnt = new int[128];
-        String ans = "";
-        int minLen = (int) (1e9);
-        int unique = 0;
+        int distincet = 0;
         for (char c : t.toCharArray()) {
             if (cnt[c] == 0) {
-                unique++;
+                distincet++;
             }
             cnt[c]++;
         }
         int left = 0, right = 0;
-        int sLen = s.length();
-        while (right < sLen) {
-            while (unique > 0 && right < sLen) {
-                char c = s.charAt(right);
-                cnt[c]--;
-                if (cnt[c] == 0) {
-                    unique--;
+        String ans = "";
+        while (right < s.length()) {
+            while (right < s.length() && distincet > 0) {
+                cnt[s.charAt(right)]--;
+                if (cnt[s.charAt(right)] == 0) {
+                    distincet--;
                 }
                 right++;
             }
-            while (unique == 0) {
-                char c = s.charAt(left);
-                cnt[c]++;
-                if (cnt[c] > 0) {
-                    unique++;
+            while (left <= right && distincet == 0) {
+                if (cnt[s.charAt(left)] == 0) {
+                    ans = (ans.isEmpty() || left < right && ans.length() > (right - left)) ? s.substring(left, right) : ans;
+                    distincet++;
                 }
+                cnt[s.charAt(left)]++;
                 left++;
             }
-            if (left>0) {
-                int len = right - left+1;
-                if (len < minLen) {
-                    minLen = len;
-                    ans = s.substring(left-1, right);
-                }
-            }
+
         }
         return ans;
     }
